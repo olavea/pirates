cd2308521f1c989b6e74c1cc40bada7abd4c12bf
olavea <ola@olavea.com>
2025-02-02 17:40:28 +0100

    Illu #1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,7 +67,7 @@
                             >
                                 <div id="screenshot-container" class="relative flex w-full flex-1 items-stretch">
                                     <img
-                                        src="https://laravel.com/assets/img/welcome/docs-light.svg"
+                                        src="https://github.com/user-attachments/assets/c3d2e3e4-ed33-4a50-a557-06346034a82e"
                                         alt="Laravel documentation screenshot"
                                         class="aspect-video h-full w-full flex-1 rounded-[10px] object-top object-cover drop-shadow-[0px_4px_34px_rgba(0,0,0,0.06)] dark:hidden"
                                         onerror="
@@ -78,7 +78,7 @@
                                         "
                                     />
                                     <img
-                                        src="https://laravel.com/assets/img/welcome/docs-dark.svg"
+                                        src="https://github.com/user-attachments/assets/c3d2e3e4-ed33-4a50-a557-06346034a82e"
                                         alt="Laravel documentation screenshot"
                                         class="hidden aspect-video h-full w-full flex-1 rounded-[10px] object-top object-cover drop-shadow-[0px_4px_34px_rgba(0,0,0,0.25)] dark:block"
                                     />
